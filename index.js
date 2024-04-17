c49edd37aa0976413815f7bf911824ffb3304cf6
Dakota <dakotaday1997@gmail.com>
2024-04-17 10:57:13 -0500

    Added new tweets to function

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ $(document).ready(() => {
   const $body = $('body');
   $body.html('');
 
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+  function addNewTweets(){
+    const $tweets = streams.home.map((tweet) => {
+      const $tweet = $('<div></div>');
+      const text = `@${tweet.user}: ${tweet.message}`;
 
-    $tweet.text(text);
+      $tweet.text(text);
 
-    return $tweet;
-  });
+      return $tweet;
+    });
   $body.append($tweets);
+  return $tweets
+  }
+  addNewTweets()
+  
+  
 
 });
