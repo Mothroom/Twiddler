29613ed991cc6821958229243683e4c3cd0ce27b
Dakota <dakotaday1997@gmail.com>
2024-04-19 17:59:08 -0500

    Added for styling

--- index.js
+++ index.js
@@ -9,9 +9,10 @@ $(document).ready(() => {
     $(window).scrollLeft()) + "px");
     return this;
   }
+  
 
   //!Body Style!//
-  $('body').center()
+  $('body').center();
   $('body').css('margin-top', '-390px');
   $('body').css('background-color', '#3d3d3d');
   $('body').css('color', 'white');
@@ -29,66 +30,72 @@ $(document).ready(() => {
     const $tweets = streams.home.filter(tweet => !displayedTweets[tweet.created_at])
     .sort((a, b) => new Date(b.created_at) - new Date(a.created_at))
     .map((tweet) => {
-      const $tweet = $('<div class="tweet" style="border: 3px solid black; margin-top: 10px;"></div>');
+      const $tweet = $('<div class="tweet" style="border: 4px solid black; border-radius: 10px; margin-top: 10px;"></div>');
       const $username = $(`<span class="username">@${tweet.user}</span>`);
-      const messageWithHashtags = tweet.message.replace(/#(\w+)/g, '<span class="hashtag" data-tag="$1">#$1</span>');
-      const $message = $(`<span class="message">${messageWithHashtags}</span>`);
+      const messageWithHashtags = tweet.message.replace(/#(\w+)/g, '<span class="hashtag" data-tag="$1" style="color: #add8e6">#$1</span>');
+      const $message = $(`<span class="message" >${messageWithHashtags}</span>`);
       const timestamp = new Date(tweet.created_at);
-      const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}">${moment(timestamp).fromNow()}</span>`);
+      const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}" style="color: #d3d3d3">${moment(timestamp).fromNow()}</span>`);
 
       $tweet.append($username, ': ', $message, ' - ', $timestamp);
-
-
       displayedTweets[tweet.created_at] = true;
-      $tweet.find('.hashtag').css('color', '#add8e6');
-      $tweet.find('.timestamp').css('color', '#d3d3d3');
-
       return $tweet;
     });
     $body.prepend($tweets);
-    return $tweets
+    return $tweets;
   }
-  addNewTweets()
+  addNewTweets();
 
   //Fetch Tweets Button
-  $($body).before('<button id="get-more-tweets-button"> Get More Tweets!')
+  $($body).before('<button id="get-more-tweets-button"> Get More Tweets!');
   $('#get-more-tweets-button').on('click', function(){
-    $body.prepend(addNewTweets())
-  })
+    $body.prepend(addNewTweets());
+  });
   //!Button Style!//
-  $('button').center()
-  $('button').css('margin-left', '66px')
-  $('button').css('margin-top', '-407px')
+  $('button').center();
+  $('button').css('margin-left', '66px');
+  $('button').css('margin-top', '-407px');
+  $('button').css('border-radius', '15px');
 
   //Get Tweet History
   $body.on('click', '.username', function(){
     const username = $(this).text().replace(/^@/, '');
-    userTweets(username)
+    userTweets(username);
   })
 
   //Create Tweet Form
-  $('button').before('<form action="" method="get" class="tweet-form">')
-  $('.tweet-form').append(`<div class='tweet-form-username'></div>`)
-  $('.tweet-form-username').append(`<label for='username' class='username'><b>Username:</label>`)
-  $('.tweet-form-username').append(`<input type='text' name='username' id='username' required />`)
+  $('button').before('<form action="" method="get" class="tweet-form">');
+  $('.tweet-form').append(`<div class='tweet-form-username'></div>`);
+  $('.tweet-form-username').append(`<label for='username' class='username'><b>Username:</label>`);
+  $('.tweet-form-username').append(`<input type='text' name='username' id='username' required />`);
 
-  $('.tweet-form').append(`<div class='tweet-form-tweet'></div>`)
-  $('.tweet-form-tweet').append(`<label for='tweet' class='tweet'><b>Speak Your Mind:</label>`)
-  $('.tweet-form-tweet').append(`<input type='text' name='tweet' id='tweet' required />`)
+  $('.tweet-form').append(`<div class='tweet-form-tweet'></div>`);
+  $('.tweet-form-tweet').append(`<label for='tweet' class='tweet'><b>Speak Your Mind:</label>`);
+  $('.tweet-form-tweet').append(`<input type='text' name='tweet' id='tweet' required />`);
 
-  $('.tweet-form').append(`<div class='tweet-form-submit'></div>`)
-  $('.tweet-form-submit').append(`<input type='submit' id='submit' />`)
+  $('.tweet-form').append(`<div class='tweet-form-submit'></div>`);
+  $('.tweet-form-submit').append(`<input type='submit' id='submit' />`);
   //!Form Style!//
-  $('form').center()
-  $('form').css('margin-left', '300px')
-  $('form').css('margin-top', '-443px')
-  $('form').css('border', '5px outset black')
-  $('#username').css('margin-left', '25.5px')
-  $('#submit').css('margin-left', '110px')
-  $('#submit').css('margin-top', '5px')
-  $('label.username').css('margin-left', '25.5px')
-  $('label').css('text-shadow', `2px 2px black`)
+  $('form').center();
+  $('form').css('margin-left', '300px');
+  $('form').css('margin-top', '-443px');
+  $('form').css('border', '5px outset black');
+  $('form').css('border-radius', '15px');
+  $('#username').css('margin-left', '25.5px');
+  $('#submit').css('margin-left', '125px');
+  $('#submit').css('margin-top', '5px');
+  $('label.username').css('margin-left', '25.5px');
+  $('label').css('text-shadow', `2px 2px black`);
   $('label').css('color', 'white');
+
+  //Twiddler Header
+  $('.tweet-form').before('<header id="page-name">TWIDDLER</header>');
+  $('#page-name').append('<img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Eastern_Bluebird.jpg" height ="100px"width="150px"/>');
+  $('#page-name').center();
+  $('#page-name').css('margin-top', '-560px');
+  $('#page-name').css('color', '#1DA1F2');
+  $('#page-name').css('font', '100px Helvetica');
+  $('header').css('border', '5px solid #1DA1F2');
 
   //Listen for the submit
   $('.tweet-form').on('submit', function(event){
@@ -111,14 +118,14 @@ $(document).ready(() => {
   function hashtags(tweets) {
     $body.empty();
     tweets.forEach(tweet => {
-      const $tweet = $('<div class="tweet"></div>');
+      const $tweet = $('<div class="tweet" style="border: 4px solid black; border-radius: 10px; margin-top: 10px;"></div>');
       const $username = $(`<span class="username">@${tweet.user}</span>`);
-      const messageWithHashtags = tweet.message.replace(/#(\w+)/g, '<span class="hashtag" data-tag="$1">#$1</span>');
+      const messageWithHashtags = tweet.message.replace(/#(\w+)/g, '<span class="hashtag" data-tag="$1" style="color: #add8e6">#$1</span>');
       const $message = $(`<span class="message">${messageWithHashtags}</span>`);
       const timestamp = new Date(tweet.created_at);
-      const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}">${moment(timestamp).fromNow()}</span>`);
+      const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}" style="color: #d3d3d3">${moment(timestamp).fromNow()}</span>`);
       $tweet.append($username, ': ', $message, ' - ', $timestamp);
-      $body.append($tweet);
+      $body.prepend($tweet);
     });
   }
 
@@ -127,20 +134,25 @@ $(document).ready(() => {
     const hashtag = $(this).text().replace(/^#/, '');
     const tweetsWithHashtag = streams.home.filter(tweet => tweet.message.includes(`#${hashtag}`));
     $body.empty()
-  
+
     // Render tweets with the same hashtag
     hashtags(tweetsWithHashtag);
   });
 
+  //Function to see one users tweets
   function userTweets(username){
     const userTweets = streams.users[username];
     $body.empty();
-
     userTweets.forEach(tweet => {
-      const $tweet = $('<div></div>');
-      const text = `@${tweet.user}: ${tweet.message} ${moment().startOf('second').fromNow()}`;
-      $tweet.text(text);
-      $body.append($tweet);
+      const $tweet = $('<div class="tweet" style="border: 4px solid black; border-radius: 10px; margin-top: 10px;"></div>');
+      const $username = $(`<span class="username">@${tweet.user}</span>`);
+      const messageWithHashtags = tweet.message.replace(/#(\w+)/g, '<span class="hashtag" data-tag="$1" style="color: #add8e6">#$1</span>');
+      const $message = $(`<span class="message">${messageWithHashtags}</span>`);
+      const timestamp = new Date(tweet.created_at);
+      const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}" style="color: #d3d3d3">${moment(timestamp).fromNow()}</span>`);
+
+      $tweet.append($username, ': ', $message, ' - ', $timestamp);
+      $body.prepend($tweet);
     })
   }
 
@@ -153,7 +165,7 @@ $(document).ready(() => {
       $timestamp.text(relativeTime);
     });
   }
-  
+
   updateTimestamps();
   setInterval(updateTimestamps, 30000);
 });
