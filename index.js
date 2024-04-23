ee342af209e44667981ce6ccd0e365dfc585de73
Dakota <dakotaday1997@gmail.com>
2024-04-23 13:58:04 -0500

    More Styling

--- index.js
+++ index.js
@@ -9,21 +9,28 @@ $(document).ready(() => {
     $(window).scrollLeft()) + "px");
     return this;
   }
-  
 
   //!Body Style!//
-  $('body').center();
-  $('body').css('margin-top', '-390px');
+  $('body').css('position', 'absolute');
+  $('body').css('left', '37%');
   $('body').css('background-color', '#3d3d3d');
   $('body').css('color', 'white');
   $('body').css('text-shadow', `2px 2px black`);
   $('body').css('font-size', '20px');
+  $('body').css('width', '30%');
 
   const $body = $('body');
   $body.html('');
 
   //Hold already displayed tweets
   const displayedTweets = {};
+  $body.append('<div class="tweet-container"></div>')
+
+  //Footer
+  $body.after(`<footer class='footer'>`)
+  $('.footer').css('background-color', 'black')
+  $('.footer').css('height', '125px')
+
 
   //Function to add tweets
   function addNewTweets(){
@@ -41,20 +48,18 @@ $(document).ready(() => {
       displayedTweets[tweet.created_at] = true;
       return $tweet;
     });
-    $body.prepend($tweets);
+    $('.tweet-container').prepend($tweets);
     return $tweets;
   }
   addNewTweets();
 
   //Fetch Tweets Button
-  $($body).before('<button id="get-more-tweets-button"> Get More Tweets!');
+  $($body).prepend('<div class="button"></div>');
+  $('.button').prepend('<button id="get-more-tweets-button"> Get More Tweets!');
   $('#get-more-tweets-button').on('click', function(){
-    $body.prepend(addNewTweets());
+    $('.tweet-container').prepend(addNewTweets());
   });
   //!Button Style!//
-  $('button').center();
-  $('button').css('margin-left', '66px');
-  $('button').css('margin-top', '-407px');
   $('button').css('border-radius', '15px');
 
   //Get Tweet History
@@ -64,7 +69,8 @@ $(document).ready(() => {
   })
 
   //Create Tweet Form
-  $('button').before('<form action="" method="get" class="tweet-form">');
+  $($body).prepend('<div class="form"></div>');
+  $('.form').prepend('<form action="" method="get" class="tweet-form">');
   $('.tweet-form').append(`<div class='tweet-form-username'></div>`);
   $('.tweet-form-username').append(`<label for='username' class='username'><b>Username:</label>`);
   $('.tweet-form-username').append(`<input type='text' name='username' id='username' required />`);
@@ -76,23 +82,21 @@ $(document).ready(() => {
   $('.tweet-form').append(`<div class='tweet-form-submit'></div>`);
   $('.tweet-form-submit').append(`<input type='submit' id='submit' />`);
   //!Form Style!//
-  $('form').center();
-  $('form').css('margin-left', '300px');
-  $('form').css('margin-top', '-443px');
   $('form').css('border', '5px outset black');
   $('form').css('border-radius', '15px');
-  $('#username').css('margin-left', '25.5px');
-  $('#submit').css('margin-left', '125px');
+  $('#username').css('margin-left', '40px');
+  $('#username').css('width', '450px');
+  $('#tweet').css('width', '450px');
+  $('#submit').css('margin-left', '320px');
   $('#submit').css('margin-top', '5px');
   $('label.username').css('margin-left', '25.5px');
   $('label').css('text-shadow', `2px 2px black`);
   $('label').css('color', 'white');
 
   //Twiddler Header
-  $('.tweet-form').before('<header id="page-name">TWIDDLER</header>');
-  $('#page-name').append('<img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Eastern_Bluebird.jpg" height ="100px"width="150px"/>');
-  $('#page-name').center();
-  $('#page-name').css('margin-top', '-560px');
+  $($body).prepend('<div class="header"></div>');
+  $('.header').prepend('<header id="page-name">TWIDDLER</header>');
+  $('#page-name').append('<img src="https://upload.wikimedia.org/wikipedia/commons/d/d5/Eastern_Bluebird.jpg" height ="100px" width="150px" style="margin-top: -17px" />');
   $('#page-name').css('color', '#1DA1F2');
   $('#page-name').css('font', '100px Helvetica');
   $('header').css('border', '5px solid #1DA1F2');
@@ -116,7 +120,7 @@ $(document).ready(() => {
 
 // Function to show tweets with the same hashtag
   function hashtags(tweets) {
-    $body.empty();
+    $('.tweet-container').empty();
     tweets.forEach(tweet => {
       const $tweet = $('<div class="tweet" style="border: 4px solid black; border-radius: 10px; margin-top: 10px;"></div>');
       const $username = $(`<span class="username">@${tweet.user}</span>`);
@@ -125,7 +129,7 @@ $(document).ready(() => {
       const timestamp = new Date(tweet.created_at);
       const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}" style="color: #d3d3d3">${moment(timestamp).fromNow()}</span>`);
       $tweet.append($username, ': ', $message, ' - ', $timestamp);
-      $body.prepend($tweet);
+      $('.tweet-container').prepend($tweet);
     });
   }
 
@@ -133,7 +137,7 @@ $(document).ready(() => {
   $body.on('click', '.hashtag', function() {
     const hashtag = $(this).text().replace(/^#/, '');
     const tweetsWithHashtag = streams.home.filter(tweet => tweet.message.includes(`#${hashtag}`));
-    $body.empty()
+    $('.tweet-container').empty()
 
     // Render tweets with the same hashtag
     hashtags(tweetsWithHashtag);
@@ -142,7 +146,7 @@ $(document).ready(() => {
   //Function to see one users tweets
   function userTweets(username){
     const userTweets = streams.users[username];
-    $body.empty();
+    $('.tweet-container').empty();
     userTweets.forEach(tweet => {
       const $tweet = $('<div class="tweet" style="border: 4px solid black; border-radius: 10px; margin-top: 10px;"></div>');
       const $username = $(`<span class="username">@${tweet.user}</span>`);
@@ -152,7 +156,7 @@ $(document).ready(() => {
       const $timestamp = $(`<span class="timestamp" data-timestamp="${timestamp}" style="color: #d3d3d3">${moment(timestamp).fromNow()}</span>`);
 
       $tweet.append($username, ': ', $message, ' - ', $timestamp);
-      $body.prepend($tweet);
+      $('.tweet-container').prepend($tweet);
     })
   }
 
@@ -168,4 +172,12 @@ $(document).ready(() => {
 
   updateTimestamps();
   setInterval(updateTimestamps, 30000);
+
+  //Mouse pointer
+  $('span.username').hover(function() {
+    $(this).css('cursor','pointer');
+  });
+  $('.hashtag').hover(function() {
+    $(this).css('cursor','pointer');
+  });
 });
